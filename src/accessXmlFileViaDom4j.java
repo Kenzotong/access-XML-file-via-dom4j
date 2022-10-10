4cac2190de35a225faf0de5aa48fda96d2b920a6
Kenzo <709567093tkz@gmail.com>
2022-10-10 23:34:20 +0200

    create a function

--- src/accessXmlFileViaDom4j.java
+++ src/accessXmlFileViaDom4j.java
@@ -1,20 +1,28 @@
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+//import java.util.Set;
 
 import org.dom4j.Attribute;
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
 public class accessXmlFileViaDom4j {
-    public static void main(String[]args) throws Exception{
+    public static void main(String[] args) throws DocumentException {
+
+        System.out.println(getIdAndDesc());
+
+    }
+
+
+    public static Map<Attribute, Attribute> getIdAndDesc() throws DocumentException {
+
         SAXReader reader = new SAXReader();
 
         //loading xml file, get document structure
         Document document = reader.read("D:/Jetbrain/bene/scenarios/tourismFacilities/tourismFacilities.xml");
-
         //get root element
         Element root = document.getRootElement();
 
@@ -26,16 +34,19 @@ public class accessXmlFileViaDom4j {
 
         //Iterate over the elements and assign into this hash map
         for (Element facility : facilities ){
-               Attribute id = facility.attribute("id") ;
-               Attribute desc = facility.attribute("desc");
-               idAndDesc.put(id, desc);
+            Attribute id = facility.attribute("id") ;
+            Attribute desc = facility.attribute("desc");
+            idAndDesc.put(id, desc);
         }
 
         //output
-        Set<Attribute> keySet = idAndDesc.keySet();
-        for (Attribute id : keySet){
-            System.out.println(id + " " + idAndDesc.get(id));
-        }
+//        Set<Attribute> keySet = idAndDesc.keySet();
+//        for (Attribute id : keySet){
+//            System.out.println(id + " " + idAndDesc.get(id));
+//        }
+
+        return idAndDesc;
+
 
     }
 
